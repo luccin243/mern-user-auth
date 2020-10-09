653aa3ae600d9716767f6e580e9fdf8ac72f93d4
Luccin Masirika <luccinmasirika@gmail.com>
2020-10-10 00:11:28 +0200

    v 1.0

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const path = require('path')
 
 const users = require('./routes/api/users')
 
@@ -36,6 +37,16 @@ require('./config/passport')(passport)
 
 // Routes
 app.use('/api/users', users)
+
+// Set static assets if is in production
+if (process.env.NOD_ENV === 'production') {
+  // Set static folder
+  app.user(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000
 
